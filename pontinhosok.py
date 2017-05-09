8c47755e7343cbe7f15f3df531a441fa3d18538d
thalesathayde <thales.athayde.santos@gmail.com>
2017-05-09 18:40:12 -0300

    endireitando score

--- pontinhosok.py
+++ pontinhosok.py
@@ -42,13 +42,12 @@ def score(cur_move):
             if lines[x+1][y] and columns[y][x] and columns[y+1][x]:
                 ponto+=1
     elif cur_move[0] == "c":
-        if not x==0 and not y==n-1:
-            if not x == 0 and not y == n - 1:
-                if columns[x - 1][y] and lines[y][x - 1] and lines[y + 1][x - 1]:
-                    ponto += 1
-            if not x == n and not y == n - 1:
-                if columns[x + 1][y] and lines[y][x] and lines[y + 1][x]:
-                    ponto += 1
+        if not x == 0 and not y == n - 1:
+            if columns[x - 1][y] and lines[y][x - 1] and lines[y + 1][x - 1]:
+                ponto += 1
+        if not x == n and not y == n - 1:
+            if columns[x + 1][y] and lines[y][x] and lines[y + 1][x]:
+                ponto += 1
     return ponto
 
 
